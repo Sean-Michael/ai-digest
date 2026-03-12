8d0b10e71b167fe49f4c13c8b1d214e651aedf3b
Sean-Michael <seanm.riesterer@gmail.com>
2026-03-12 16:06:46 -0700

    basic scraping with test output on articles that stumped the RSS feed ingestions

--- fetch_article.py
+++ fetch_article.py
@@ -4,6 +4,8 @@ from requests import Response, RequestException
 from bs4 import BeautifulSoup
 
 def fetch_article(url : str) -> Response | RequestException:
+    print(f"URL: {url}\n")
+
     try:
         response = requests.get(url, timeout=10)
         response.raise_for_status()
@@ -12,17 +14,28 @@ def fetch_article(url : str) -> Response | RequestException:
     
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
-        body = soup.find_all('section', id='post-body')
+        noise_tags = ['nav', 'footer', 'header', 'aside', 'script', 'meta', 'style', 'form']
 
-        print(f"Found {len(body)} <p> text")
-        for text in body:
-            print(text.get_text(separator='\n', strip=True))
+        #print(f"BEFORE:\n {soup.get_text(separator='\n', strip=True)}")
+
+        noise = soup.find_all(noise_tags)
+        if noise:
+            for n in noise:
+                n.decompose()
+        #print(f"AFTER:\n {soup.get_text(separator='\n', strip=True)}")
+
+        content = soup.find('article') or soup.find('main') or soup.find('body')
+        if content:
+            print(f"CONTENT:\n {content.get_text(separator='\n', strip=True)}")
 
     return response
 
 
 def main():
-    print(fetch_article('https://addyosmani.com/blog/factory-model/'))
+    print(fetch_article('https://www.anthropic.com/engineering/building-c-compiler'))
+    print(fetch_article('https://github.com/jrswab/axe'))
+    print(fetch_article('https://kubernetes.io/blog/2026/03/09/announcing-ai-gateway-wg/'))
+    print(fetch_article('https://huggingface.co/blog/ibm-granite/granite-4-speech'))
 
 if __name__ == "__main__":
     main()
